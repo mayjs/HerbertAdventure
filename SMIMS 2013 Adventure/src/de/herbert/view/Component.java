9c031764ac38a805d394bec4d6a956dafd9de7c0
Kemiren <thekemiren@googlemail.com>
2013-10-20 22:14:28 +0200

    Added Classes

---
+++ SMIMS 2013 Adventure/src/de/herbert/view/Component.java
@@ -0,0 +1,28 @@
+package de.herbert.view;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+
+public abstract class Component {
+	protected Rectangle boundings;
+	
+	public Component(Rectangle boundings){
+		this.boundings = boundings;
+	}
+		
+	public Rectangle getBoundings() {
+		return boundings;
+	}
+
+	public void setBoundings(Rectangle boundings) {
+		this.boundings = boundings;
+	}
+
+
+	public abstract void update(GameContainer container, int delta)
+			throws SlickException;
+	public abstract void render(GameContainer container, Graphics g)
+			throws SlickException;
+}
