239191b8fca65aa1b8b3109bc5ef92941a7142a2
Kemiren <thekemiren@googlemail.com>
2013-10-20 21:41:30 +0200

    Implemented basic game methods an main method.

--- SMIMS 2013 Adventure/src/de/herbert/control/Control.java
+++ SMIMS 2013 Adventure/src/de/herbert/control/Control.java
@@ -1,5 +1,41 @@
 package de.herbert.control;
 
-public class Control {
-	// ein Kommentar..
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Control extends BasicGame {
+
+	public Control(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) throws SlickException{
+		AppGameContainer container = new AppGameContainer(new Control("Herbert"), 800, 600, false);
+		
+		container.start();
+	}
 }
