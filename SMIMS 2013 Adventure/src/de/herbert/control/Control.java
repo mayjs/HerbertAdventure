09670edce061151ae8c44a239b6ffa47bb1547c0
Kemiren <thekemiren@googlemail.com>
2013-10-20 22:10:24 +0200

    Implemented Component System

--- SMIMS 2013 Adventure/src/de/herbert/control/Control.java
+++ SMIMS 2013 Adventure/src/de/herbert/control/Control.java
@@ -6,8 +6,12 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
+import de.herbert.view.View;
+
 public class Control extends BasicGame {
 
+	private View view;
+	
 	public Control(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
@@ -16,20 +20,20 @@ public class Control extends BasicGame {
 	@Override
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		view.render(container, g);
 		
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		// TODO Auto-generated method stub
-		
+		view = new View(this);
 	}
 
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		view.update(container, delta);
 		
 	}
 	
--- SMIMS 2013 Adventure/src/de/herbert/view/View.java
+++ SMIMS 2013 Adventure/src/de/herbert/view/View.java
@@ -1,11 +1,40 @@
 package de.herbert.view;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+
 import de.herbert.control.Control;
 
 public class View {
 	private Control control;
 	
+	private List<Component> components;
+	
 	public View(Control control){
 		this.control = control;
+		
+		components = new LinkedList<Component>();
+		
+		//Add components here!
+		components.add(new SimpleComponent(new Rectangle(30,30,50,50)));
+	}
+	
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		for(Component c : components){
+			c.update(container, delta);
+		}
+	}
+	
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		for(Component c : components){
+			c.render(container, g);
+		}
 	}
 }
