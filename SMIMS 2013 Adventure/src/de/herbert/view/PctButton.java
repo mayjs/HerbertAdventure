2f2eec4475bd301c2062e8586b547a250c8e7943
Kemiren <thekemiren@googlemail.com>
2013-10-21 22:09:27 +0200

    +PctButton now has a setImage(Image) method ~Images get rendered in the center of the button now

--- SMIMS 2013 Adventure/src/de/herbert/view/PctButton.java
+++ SMIMS 2013 Adventure/src/de/herbert/view/PctButton.java
@@ -9,19 +9,40 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 
 public class PctButton extends Button{
-	Image img;
-	
+	private Image img;
+	private float renderX, renderY;
 	
 	public PctButton(Rectangle boundings, Image image){
 		super(boundings);
-		img = image;
-		float scale = (float)0.5;
-		if((img.getWidth() - getBoundings().getWidth()) > (img.getHeight() - getBoundings().getHeight())){
-			scale = (getBoundings().getWidth() - 2*GAP) / img.getWidth();
-		}else{
-			scale = (getBoundings().getHeight() - 2*GAP)/  img.getHeight();
+//		img = image;
+//		float scale = (float)0.5;
+//		if((img.getWidth() - getBoundings().getWidth()) > (img.getHeight() - getBoundings().getHeight())){
+//			scale = (getBoundings().getWidth() - 2*GAP) / img.getWidth();
+//		}else{
+//			scale = (getBoundings().getHeight() - 2*GAP)/  img.getHeight();
+//		}
+//		img = img.getScaledCopy(scale);
+		setImage(image);
+	}
+	
+	public void setImage(Image pImg){
+		img = pImg;
+		if(img.getWidth() > boundings.getWidth() - 2*this.GAP || img.getHeight() > boundings.getHeight() - 2*this.GAP){
+			float scale = 1f;
+			if((img.getWidth() - getBoundings().getWidth()) > (img.getHeight() - getBoundings().getHeight())){
+				scale = (getBoundings().getWidth() - 2*GAP) / img.getWidth();
+			}else{
+				scale = (getBoundings().getHeight() - 2*GAP)/  img.getHeight();
+			}
+			img = img.getScaledCopy(scale);
 		}
-		img = img.getScaledCopy(scale);
+		
+		Rectangle imgBoundings = new Rectangle(boundings.getX()+GAP,boundings.getY()+GAP,img.getWidth(),img.getHeight());
+		float dx = boundings.getCenterX() - imgBoundings.getCenterX();
+		float dy = boundings.getCenterY() - imgBoundings.getCenterY();
+		
+		renderX = boundings.getX() + GAP + dx;
+		renderY = boundings.getY() + GAP + dy;
 	}
 	
 	public void update(GameContainer container, int delta)
@@ -31,8 +52,9 @@ public class PctButton extends Button{
 	
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
-		// background is updated in super.render(...)
+		// background is rendered in super.render(...)
 		super.render(container, g);
-		g.drawImage(img, getBoundings().getX() + GAP, getBoundings().getY() + GAP);
+//		g.drawImage(img, getBoundings().getX() + GAP, getBoundings().getY() + GAP);
+		g.drawImage(img, renderX, renderY);
 	}
 }
